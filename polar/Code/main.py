041ec03f50f025664409b65f846a5d9f56164a36
cow-coding <kbp0237@gmail.com>
2022-02-22 16:38:58 +0000

    [File add] EDA File update

--- polar/Code/main.py
+++ polar/Code/main.py
@@ -1,3 +1,5 @@
+from utils import *
+
 import os
 import argparse
 
@@ -40,48 +42,6 @@ def args_getter():
     return args
 
 
-def make_img_path_df(path, output_dir=None, train=True):
-    data_path = '/opt/ml/input/data'
-
-    if train:
-        df = pd.DataFrame(None, columns=['path', 'class'])
-        data_path += '/train/train.csv'
-        train_df = pd.read_csv(data_path)
-
-        for idx, row in enumerate(train_df.iloc):
-            for file in list(os.listdir(os.path.join(path, row['path']))):
-                if file[0] == '.':
-                    continue
-                else:
-                    file_name = file.split('.')[0]
-
-                    if file_name == "normal":
-                        mask = 2
-                    elif file_name == "incorrect_mask":
-                        mask = 1
-                    else:
-                        mask = 0
-                gender = 0 if row['gender'] == "male" else 1
-                data = {
-                    'path': os.path.join(path, row['path'], file),
-                    'class':mask*6 + gender*3 + min(2, row['age']//30)
-                }
-                df = df.append(data, ignore_index=True)
-
-        df.to_csv(output_dir, index=False)
-
-
-def record_expr(model, model_name, best_train_loss, best_train_score, avg_val_loss, avg_val_score, args):
-    # | Date | model_name | best_loss | f1 score | avg val loss | avg val f1 score | Hyperparameters |
-    base_url = '/opt/ml'
-    model_state_save_path = base_url+'/level1-image-classification-level1-recsys-12/polar/model_state'
-    markdown_path = base_url+'/level1-image-classification-level1-recsys-12/polar/README.md'
-    current_time = datetime.now(pytz.timezone('Asia/Seoul')).strftime("%Y-%m-%d %H:%M:%S")
-
-    os.system(f"echo '|{current_time}|{model_name}|{best_train_loss:.3g}|{best_train_score:.3g}|{avg_val_loss:.3g}|{avg_val_score:.3g}|{str(vars(args))}|' >> {markdown_path}")
-    print("Save the experiment complete!")
-
-
 def main(args):
     # directories
     base_dir = '/opt/ml'
@@ -121,7 +81,7 @@ def main(args):
     valid_dataset = CustomDataSet(list(X_valid['path'].values), list(y_valid.values), transform)
 
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2)
-    val_loader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2)
+    val_loader = DataLoader(valid_dataset, shuffle=False, num_workers=2)
 
     # Model part
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -209,8 +169,8 @@ def main(args):
             counts += 1
 
         acc = corrects / len(valid_dataset)
-        f1 = f1 / counts
-        test_loss = test_loss/counts
+        f1 = f1 / len(valid_dataset)
+        test_loss = test_loss/len(valid_dataset)
 
         print(f"Avg Accuracy : {acc :.3g} | Avg F1 Score {f1 : .3g} - Avg loss : {test_loss:.5g}")
 
